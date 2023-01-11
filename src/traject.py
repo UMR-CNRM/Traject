6ad422d124c2ac43b40037b53a55e3122f5b6a45
Matthieu Plu <matthieu.plu@meteo.fr>
2023-01-11 07:56:31 +0100

    v0.81

--- src/traject.py
+++ src/traject.py
@@ -28,7 +28,7 @@ import Inputs, Tools
 import epygram
 
 #Generic variables
-traject_version = 0.8
+traject_version = 0.81
 missval = -9999.0
 time_orig = np.datetime64("1970-01-01T00:00:00")
 time_fmt = "%Y%m%d%H"
